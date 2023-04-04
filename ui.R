4508fc1a0968b7e87a7b0c088b536d93110946dd
Ben Cullen <Bencullenn@gmail.com>
2023-04-04 17:50:46 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,26 @@
+library(shiny)
 
+# Define UI for the app
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Basic Shiny App"),
+  
+  # Sidebar with controls
+  sidebarLayout(
+    sidebarPanel(
+      # Add input controls
+      sliderInput("num",
+                  "Number of observations:",
+                  min = 1,
+                  max = 100,
+                  value = 50)
+    ),
+    
+    # Main panel for displaying output
+    mainPanel(
+      # Add output display
+      plotOutput("scatterPlot")
+    )
+  )
+))
